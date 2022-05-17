9f02315ea623e5bf7e52f6962ae66bad07076c20
김태용 <amico741@naver.com>
2022-05-17 21:35:23 +0900

    Fix: BOJ_1987
    딕셔너리 -> 리스트로 변경

--- yongshub-python/backtracking/BOJ_6603.py
+++ yongshub-python/backtracking/BOJ_6603.py
@@ -15,8 +15,8 @@ while True:
 def solution(k, testCase):
     if(k == 6):  # 6번째라면
         res = arr[:6]
-        res.sort() # 정렬 후
-        result.append(tuple(res)) # result에 추가
+        res.sort()  # 정렬 후
+        result.append(tuple(res))  # result에 추가
         return None
 
     for i in testCase:
@@ -28,9 +28,9 @@ def solution(k, testCase):
 
 
 for testCase in testCases:
-    solution(0, testCase[1:])
-    result = set(result) # 중복된 요소 제거
-    result = sorted(result) # 정렬
+    solution(0, testCase[1:])  # 1:
+    result = set(result)  # 중복된 요소 제거
+    result = sorted(result)  # 정렬
     for res in result:
         print(*res)
     print()
