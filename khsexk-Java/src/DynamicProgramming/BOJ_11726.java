c0456667d222bffe94eb5e67b6127e97bf33a83f
khsexk <kohyunsuk98@gmail.com>
2022-05-27 17:56:51 +0900

    BOJ 11726 2xn 타일링

---
+++ khsexk-Java/src/DynamicProgramming/BOJ_11726.java
@@ -0,0 +1,26 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11726 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[1001];
+		dp[0] = 0;
+		
+		for(int i=1 ; i<=n ; i++) {
+			if(i==1)  dp[1] = 1;
+			else if(i==2)  dp[2] = 2;
+			
+			else {
+				dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
