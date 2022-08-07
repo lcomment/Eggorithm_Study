c497867224ab91cf3ca298fb77d58b94d730bf22
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-08-08 00:48:06 +0900

    Update 1038.java

--- himJJong - Java/08.09/1038.java
+++ himJJong - Java/08.09/1038.java
@@ -1,31 +1,42 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import java.util.*;
+import java.io.*;
+ 
+public class Main{   
+    static ArrayList<Long> list;
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        list = new ArrayList<>();
+        
+        
+        if(num<=10) {
+        	System.out.println(num);
+        }
+        else if(num>=1023) {
+        	System.out.println(-1);
+        }
+        else {
+        	for(int i=0; i<10;i++) {    //0~부터 9까지 앞자리 개수 세기 
+        		cal(i,1);				//해서 depth10보다 크거나,for문이 다돌면 
+        	}
+        	Collections.sort(list);		//크기대로 정렬후에 
+        	System.out.println(list.get(num));	//원하는 num값 뽑아오
+        }
+    }
+    private static void cal(long num, int depth) { 	//이쪽 Long을 넣어야 하는것을 인지하는게 오래걸렸다.
+    	if(depth>10) {			//9876543210 10자리 
+    		return;
+    	}
+    	list.add((long)num);			
+    	
+    	for(int i=0; i<num%10;i++) {	//앞자리보다 작은 수가 나오면 끝까지 검색 후 나오게 ,신기한거 [num%10  236 ms] | [ 10 220ms ]
+    		if(num%10 >i) {			 
+    			cal((num*10)+i, depth+1);
+    			}
+    	}
+    	return;
+    }
+}
 
 
 
