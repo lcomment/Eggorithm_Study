10fb62dcede52306cf1cf1053d202874bd77aa13
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2022-08-16 17:57:55 +0900

    Update 14226(이모티콘).java

--- himJJong - Java/08.15/14226(이모티콘).java
+++ himJJong - Java/08.15/14226(이모티콘).java
@@ -2,14 +2,22 @@ package chapter2;
 import java.util.*;
  
 public class TypeInference {
-    
+	public static class Node {
+        int clipboard;
+        int total;
+        int time;
+        
+        public Node(int clipboard, int total, int time) {
+            this.clipboard = clipboard;
+            this.total = total;
+            this.time = time;
+        }
+    }
     static boolean[][] visited = new boolean[1001][1001];      //[clipboard][total]
     
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int s = scan.nextInt();
-        
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
         bfs(s);
     }
     
@@ -20,41 +28,26 @@ public class TypeInference {
         
         while(!q.isEmpty()) {
             Node current = q.poll();
-            
             if(current.total == s) {
                 System.out.println(current.time);
                 return;
             }
             
-            // 1. 화면에 있는 이모티콘 클립보드에 저장
-            q.offer(new Node(current.total, current.total, current.time + 1)); 
             
+            q.offer(new Node(current.total, current.total, current.time + 1));   // 화면에 있는 이모티콘 클립보드에 저장
             
-            // 2. 클립보드에 있는 이모티콘 붙여넣기. 
-            // 클립보드 비어있지 않아야하고, 붙여넣은 후 개수가 총 개수보다 적어야 하며, 이전에 방문한적 없어야함.
+
+                                                                     // 클립보드 값이 있어야하고 , 붙여넣은 후 개수가 총 개수보다 적고 이전에 방문한적 false
             if(current.clipboard != 0 && current.total + current.clipboard <= s && !visited[current.clipboard][current.total + current.clipboard]) {
                 q.offer(new Node(current.clipboard, current.total + current.clipboard, current.time + 1));
                 visited[current.clipboard][current.total + current.clipboard] = true;
             }
             
-            // 3. 화면에 있는 이모티콘 중 하나 삭제.
-            // 총 개수 1보다 크거나 같아야하고, 방문한적 없어야함.
+                                                                     // 총 개수는 1보다 크거나 같아야하고, 방문한적 false
             if(current.total >= 1 && !visited[current.clipboard][current.total - 1]) {
                 q.offer(new Node(current.clipboard, current.total - 1, current.time + 1));
                 visited[current.clipboard][current.total - 1] = true;
             }
-        }
-    }
-    
-    public static class Node {
-        int clipboard;
-        int total;
-        int time;
-        
-        public Node(int clipboard, int total, int time) {
-            this.clipboard = clipboard;
-            this.total = total;
-            this.time = time;
         }
     }
 }
