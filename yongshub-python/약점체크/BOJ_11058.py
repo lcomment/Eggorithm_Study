850ab11046268106c1b3743086b5badb00e75cf0
김태용 <amico741@naver.com>
2022-09-06 02:35:32 +0900

    BOJ_11058 크리보드
    코드 수정

--- yongshub-python/약점체크/BOJ_11058.py
+++ yongshub-python/약점체크/BOJ_11058.py
@@ -1,18 +1,8 @@
 import sys
 input = sys.stdin.readline
+n = int(input())
+dp = [i for i in range(0, 102)]
 
-n = int(input().rstrip())
-dp = [[0] * (n + 1) for _ in range(3)] # 0행 붙이기, 1행 전체 선택, 2행 복사하기
-dp[0][0] = 1
-is_select = False
-for i in range(1, n):
-    for j in range(3):
-        if j == 0: # 붙이기
-            dp[j][i] = max(dp[j][i - 1] + 1, dp[j][i - 3] + dp[j + 1][i - 1])
-        elif j == 1: # 복사하기
-            if is_select:
-                dp[j][i] = dp[j + 1][i - 1]
-        else:
-            is_select = True
-            dp[j][i] = dp[0][i - 1]
-print(dp[0][n - 1])
+for i in range(6, 101):
+    dp[i] = max(dp[i-3]*2, max(dp[i-4]*3, dp[i-5]*4))
+print(dp[n])
