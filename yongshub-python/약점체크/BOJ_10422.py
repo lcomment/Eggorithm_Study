5b3e65b97320d445f61177fa7072a5728089f87e
김태용 <amico741@naver.com>
2022-09-26 20:02:58 +0900

    PG_두 큐 합 같게 만들기

--- yongshub-python/약점체크/BOJ_10422.py
+++ yongshub-python/약점체크/BOJ_10422.py
@@ -1,16 +1,10 @@
-import sys
-input = sys.stdin.readline
-
-t = int(input())
-lst = []
-for _ in range(t):
-    lst.append(int(input()))
-
-dp = [0] * 5001
+dp = [0 for _ in range(5001)]
 dp[0] = 1
-for i in range(2, 5001, 2):
-    for j in range(2, i + 1, 2):
-        dp[i] += (dp[j - 2] * dp[i - j]) % 1000000007
 
-for i in lst:
-    print(dp[i] % 1000000007)
+for n in range(2, 5001, 2):
+    for i in range(2, n + 1, 2):
+        dp[n] += dp[i - 2] * dp[n - i]
+    dp[n] %= 1000000007
+
+for _ in range(int(input())):
+    print(dp[int(input())])
