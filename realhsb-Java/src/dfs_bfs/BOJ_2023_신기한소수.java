413fd7241f31a8dd12cdc0b1487c4c06c914acf7
hansubin <realhsb@gmail.com>
2022-09-29 23:32:22 +0900

    BOJ 1806 부분합

---
+++ realhsb-Java/src/dfs_bfs/BOJ_2023_신기한소수.java
@@ -0,0 +1,25 @@
+package dfs_bfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_2023_신기한소수 {
+	static int count = 0;
+	static int N = 0;
+	static int[] num;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+		
+	}
+	
+	static void DFS(int n, int index) {
+		if (index == 0) count = 0;
+		
+	}
+}
