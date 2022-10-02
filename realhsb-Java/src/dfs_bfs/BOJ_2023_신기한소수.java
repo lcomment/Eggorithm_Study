239a247c186b64ed770fcada166131c7752740e1
hansubin <realhsb@gmail.com>
2022-10-02 18:58:38 +0900

    BOJ 2023 신기한소수

--- realhsb-Java/src/dfs_bfs/BOJ_2023_신기한소수.java
+++ realhsb-Java/src/dfs_bfs/BOJ_2023_신기한소수.java
@@ -1,25 +1,42 @@
 package dfs_bfs;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class BOJ_2023_신기한소수 {
-	static int count = 0;
 	static int N = 0;
-	static int[] num;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
 		
-		N = Integer.parseInt(br.readLine());
-		
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
 	}
 	
 	static void DFS(int n, int index) {
-		if (index == 0) count = 0;
+		if (index == N) {
+			if(isPrime(n)) {
+				System.out.println(n);
+			}
+			return;
+		}
 		
+		for(int i = 1; i < 10; i++) {
+			if(i % 2 == 0) {
+				continue;
+			}
+			
+			if(isPrime(n * 10 + i)) {
+				DFS(n * 10 + i, index+1);
+			}
+		}
+	}
+	
+	static boolean isPrime(int n) {
+		for(int i = 2; i <= n/2; i++) {
+			if(n % i == 0) return false;
+		}
+		return true;
 	}
 }
