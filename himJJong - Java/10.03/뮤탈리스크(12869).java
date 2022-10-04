333b1d7e15eaa8915b06a1b04ff3a24e994490c3
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2022-10-04 18:32:14 +0900

    Update 뮤탈리스크(12869).java

--- himJJong - Java/10.03/뮤탈리스크(12869).java
+++ himJJong - Java/10.03/뮤탈리스크(12869).java
@@ -1,63 +1,65 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int N;
-    static int [][][] DP =  new int[61][61][61];
-    //뮤탈의 공격 경우의 수
-    static int[][] attack = { {9, 3, 1}, {9, 1, 3}, {3, 9, 1}, {3, 1, 9}, {1, 9, 3}, {1, 3, 9}};
-    static Integer[] scv = new Integer[3];	//scv 체력 저장 배열
-    public static void main(String[] args) throws IOException {
-        //입력값 처리하는 BufferedReader
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //결과값 출력하는 BufferedWriter
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        //DP[][][] 초기화
-        for(int i=0;i<61;i++){
-            for(int j=0;j<61;j++)
-                Arrays.fill(DP[i][j], Integer.MAX_VALUE);
-        }
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        //입력되는 SCV 체력 저장
-        for(int i=0;i<N;i++)
-            scv[i] = Integer.parseInt(st.nextToken());
+	static int N;
+	static int[][] deltas = {{-9,-3,-1},{-9,-1,-3},{-3,-9,-1},{-3,-1,-9},{-1,-9,-3},{-1,-3,-9}};
+	static int[][][] dp;
+	static int min;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] scv = new int[3];
+		
+		for(int i=0;i<N;i++) {
+			scv[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp = new int[61][61][61];
+		min = Integer.MAX_VALUE;
+		
+		dfs(scv,0);
+		
+		System.out.println(min);
+		
+	}
+	
+	public static void dfs(int[] scv, int cnt) {
 
-        //SCV의 개수가 3개 미만인 경우 없는 SCV의 체력 0으로 초기화
-        for(int i=N;i<3;i++)
-            scv[i] = 0;
+		int s1 = scv[0];
+		int s2 = scv[1];
+		int s3 = scv[2];
+		
+		// 공격 횟수 최솟값 보다 현재 공격 횟수가 같거나 클 경우 중단
+		if(min <= cnt) return;
+		
+		// 이미 방문했는데 기존 공격 횟수가 더 작을 경우 중단
+		if(dp[s1][s2][s3] != 0 && dp[s1][s2][s3] <= cnt) 
+			return;
+		
+		dp[s1][s2][s3] = cnt;
+		
+		// 모든 scv가 죽을 경우 최솟값 갱신 및 중단
+		if(s1 == 0 && s2 == 0 && s3 == 0) {
+			min = Math.min(min, cnt);
+			return;
+		}
+		
+		// 6가지 공격 패턴으로 현재 scv를 공격한 후 넘김
+		for(int i=0;i<6;i++) {
+			dfs(new int[] 
+					{Math.max(s1 + deltas[i][0], 0),Math.max(s2 + deltas[i][1], 0),Math.max(s3 + deltas[i][2], 0)}, cnt+1);
+		}
+	}
 
-        bw.write(search(scv.clone(),0) + "");	//최소 공격횟수 구하기 및 BufferedWriter 저장
-        bw.flush();		//결과 출력
-        bw.close();
-        br.close();
-    }
-    //뮤탈리스크 공격 경우의 수와 DP를 이용하여 최소 공격횟수 구하는 함수
-    static int search(Integer[] scv, int count){
-        boolean check = false;
-        for(int i=0;i<N;i++){	//모든 SCV가 체력이 0일 때
-            if(scv[i]!=0){
-                check = true;
-                break;
-            }
-        }
-        if(!check){	//모든 SCV가 체력이 0일 때
-            return count;
-        }else{	//모든 SCV가 체력이 0이 아닐 때
-            Arrays.sort(scv, Collections.reverseOrder());	//SCV 체력 내림차순 정렬
-            //먼저 방문했을 경우 해당 값 반환
-            if(DP[scv[0]][scv[1]][scv[2]]!=Integer.MAX_VALUE)
-                return DP[scv[0]][scv[1]][scv[2]];
-
-            //뮤탈리스크 공격 진행!
-            for(int i=0;i<6;i++){
-                Integer[] tempScv = new Integer[3];
-                tempScv[0] = Math.max(scv[0] - attack[i][0], 0);
-                tempScv[1] = Math.max(scv[1] - attack[i][1], 0);
-                tempScv[2] = Math.max(scv[2] - attack[i][2], 0);
-                //모든 공격의 최소값 DP의 저장
-                DP[scv[0]][scv[1]][scv[2]] = Math.min(DP[scv[0]][scv[1]][scv[2]], search(tempScv, count+1));
-            }
-        }
-        return DP[scv[0]][scv[1]][scv[2]];
-    }
 }
