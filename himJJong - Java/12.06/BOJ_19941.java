25226c2a48ef30577d64af37241e4123ae363738
himjjong <jjijji5728@tukorea.ac.kr>
2022-12-07 03:20:32 +0900

    BOJ_19941 햄버거 분배 주석달기 및 변수제거

--- himJJong - Java/12.06/BOJ_19941.java
+++ himJJong - Java/12.06/BOJ_19941.java
@@ -12,23 +12,23 @@ public class BOJ_19941 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         list = new ArrayList<>();
 
+        // stream 사용으로 N과 M 데이터 입력
         int[] NM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         N = NM[0];
         M = NM[1];
 
-        check = new boolean[N];
+        check = new boolean[N];             //햄버거 체크해줄 boolean
         String data = br.readLine();
-        for(int i=0 ; i<N; i++){
+        for(int i=0 ; i<N; i++){            //list에 데이터 하나씩 넣어주면서, "P"인 데이터위치의 boolean은 true로 미리 변경
             list.add(String.valueOf(data.charAt(i)));
             if(list.get(i).equals("P")) check[i] = true;
         }
 
-        while(index != N){
+        while(index != N){                  //햄버거 개수만큼 다 확인할건데
             int num = 0;
-            if(list.get(index).equals("P")) {
-                num = index;
-                for(int i=num-M ; i<=num+M; i++){
-                    if(i >= 0 && i <=N-1 && !check[i]) {
+            if(list.get(index).equals("P")) {       //해당 list의 배열값이 "P" 라면
+                for(int i=index-M ; i<=index+M; i++){
+                    if(i >= 0 && i <=N-1 && !check[i]) {    //i값을 범위내로 체킹해주며 false라면
                         check[i]=true;
                         answer++;
                         break;
@@ -37,6 +37,7 @@ public class BOJ_19941 {
             }
             index++;
         }
+
         System.out.println(answer);
     }
 }
