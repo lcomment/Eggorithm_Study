437d11d2413e303b2bb276ce7039db39a7129740
himjjong <jjijji5728@tukorea.ac.kr>
2023-01-05 14:06:07 +0900

    BOJ_1926_solo_dfs/bfs

--- himJJong - Java/01.03/BOJ_1926_solo_DFS.java
+++ himJJong - Java/01.03/BOJ_1926_solo_DFS.java
@@ -1,19 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Coord {
-    private int row;
-    private int col;
-
-    public Coord(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-    public int getRow() { return row; }
-    public int getCol() { return col; }
-}
-
-public class BOJ_1926_solo {
+public class BOJ_1926_solo_DFS {
     static int n, m;                // n: 도화지 세로 크기, m: 도화지 가로 크기
     static boolean[][] paper;       // 가로 m, 세로 n 도화지
     static boolean[][] check;       // 방문 확인
@@ -23,37 +11,8 @@ public class BOJ_1926_solo {
     static int area = 0;                // 탐색한 그림의 넓이
 
     static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };      // 상하좌우
+    static int[] dy = { 0, 1, 0, -1};      // 상하좌우
 
-
-    public static void bfs(int row, int col) {
-        Queue<Coord> queue = new LinkedList<>();
-
-        queue.add(new Coord(row, col));
-        check[row][col] = true;
-        area++;
-
-        while (!queue.isEmpty()) {
-            Coord c = queue.remove();
-
-            // 상하좌우 확인
-            for (int i = 0; i < 4; i++) {
-                int nextRow = c.getRow() + dy[i];
-                int nextCol = c.getCol() + dx[i];
-
-                // 1. 다음 지점이 도화지 범위 안이고
-                if (0 <= nextRow && nextRow < n &&
-                        0 <= nextCol && nextCol < m) {
-                    // 2. 그림이고 아직 방문 안한 경우
-                    if (paper[nextRow][nextCol] && !check[nextRow][nextCol]) {
-                        queue.add(new Coord(nextRow, nextCol));
-                        check[nextRow][nextCol] = true;
-                        area++;
-                    }
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(
@@ -81,8 +40,8 @@ public class BOJ_1926_solo {
                 // 해당 지점이 그림이고 아직 방문 안한 경우, 탐색 수행
                 if (paper[i][j] && !check[i][j]) {
                     numberOfPicture++;
-                    bfs(i, j);
-                    // dfs(i, j);
+                    dfs(i, j);
+                    // bfs(i, j);
 
                     maxArea = Math.max(maxArea, area);
                     area = 0;       // area 초기화
@@ -92,5 +51,23 @@ public class BOJ_1926_solo {
 
         System.out.println(numberOfPicture);
         System.out.println(maxArea);
+    }
+    public static void dfs(int row, int col) {
+        check[row][col] = true;
+        area++;
+
+        // 상하좌우 확인
+        for (int i = 0; i < 4; i++) {
+            int nextRow = row + dx[i];
+            int nextCol = col + dy[i];
+
+            // 1. 다음 지점이 도화지 범위 안이고
+            if (0 <= nextRow && nextRow < n &&
+                    0 <= nextCol && nextCol < m) {
+                // 2. 그림이고 아직 방문 안한 경우
+                if (paper[nextRow][nextCol] && !check[nextRow][nextCol])
+                    dfs(nextRow, nextCol);
+            }
+        }
     }
 }
