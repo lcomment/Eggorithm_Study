b89bbf6aad31a491f7608e5b5fa3ca101a638985
himjjong <jjijji5728@tukorea.ac.kr>
2023-01-12 11:17:52 +0900

    BOJ_20006 랭킹전 대기열 주석 추가

--- himJJong - Java/01.10/BOJ_20006.java
+++ himJJong - Java/01.10/BOJ_20006.java
@@ -33,7 +33,7 @@ public class BOJ_20006 {
             String id = st.nextToken();
             boolean flag = false;
             for(Room room : rooms) {
-                if(room.users.size() >= roomSize) continue;
+                if(room.users.size() >= roomSize) continue;         // roomSize 크기 이상이라면 새로운 room 만들기 위해 continue
                 if(!room.users.isEmpty() && room.users.get(0).level - 10
                         <= level && level <= room.users.get(0).level + 10) {
                     flag = true;
@@ -41,20 +41,20 @@ public class BOJ_20006 {
                     break;
                 }
             }
-            if(!flag){
-                Room room = new Room();
+            if(!flag){                                      //초기 방이 없거나, 해당 조건을 만족하지 못하여 새로운 room 생성 후
+                Room room = new Room();                     //rooms에 room 추가
                 room.users.add(new User(level, id));
-                rooms.add(room);
+                rooms.add(room);                            //전체 각각의 방들을 모아놓은 ArrayList<room> rooms
             }
         }
 
-        for (Room room : rooms) {
-            Collections.sort(room.users);
-            if(room.users.size() == roomSize )
+        for (Room room : rooms) {                           //전체 rooms들 중에서
+            Collections.sort(room.users);                   //Room 클래스 내 List<User> 중 User 클레스 타입내 필드인 level,id가 있고
+            if(room.users.size() == roomSize )              //이 중 comparable 사용으로 id로 정렬
                 bw.write("Started!\n");
             else
                 bw.write("Waiting!\n");
-            for(int i=0;i<room.users.size();i++)
+            for(int i=0;i<room.users.size();i++)            //각각 room의 정보 내에 저장된 level 과 id 값을 bw.wirte
             {
                 int level = room.users.get(i).level;
                 String nickName = room.users.get(i).nickName;
@@ -62,6 +62,6 @@ public class BOJ_20006 {
                 bw.write(level + " " + nickName+"\n");
             }
         }
-        bw.flush();
+        bw.flush();                                         //후 출력
     }
 }
