64d98451d6d3adceb3c35f680776e0e53dfc534b
lcomment <kohyunsuk98@gmail.com>
2023-03-08 09:08:04 +0900

    BOJ 2879 코딩은 예쁘게 (수정)

--- lcomment-java/src/BOJ/Greedy/BOJ_2879.java
+++ lcomment-java/src/BOJ/Greedy/BOJ_2879.java
@@ -5,32 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+/*
+ * 의문점: Max 값으로 처리할 때의 엣지 케이스를 모르겠음
+ */
 public class BOJ_2879 {
     /**
      * cur: 초기 인덴트 저장
      *
      */
     static class Line {
-        int cur, dest, move;
-        boolean tabFlag, satisfied;
+        int cur, move;
+        boolean tabFlag;
 
         Line(int cur, int dest) {
             this.cur = cur;
-            this.dest = dest;
             this.move = Math.abs(cur - dest);
 
-            // 처음부터 만족하는 인덴트일 수도 있음
-            this.satisfied = this.cur == this.dest;
             // 인덴트를 늘려야 하면 true, 줄여야 하면 false
             this.tabFlag = dest >= cur;
-        }
-
-        void tab(int n) {
-            this.satisfied = this.cur + n == dest;
-        }
-
-        void unTab(int n) {
-            this.satisfied = this.cur - n == dest;
         }
     }
     static int N, result = 0;
@@ -46,57 +38,29 @@ public class BOJ_2879 {
         current = sToIntArray(br.readLine());
         destination = sToIntArray(br.readLine());
 
+        // 하나 밖에 없는 경우
+        if(N < 2) {
+            System.out.println(destination[0] - current[0]);
+            return;
+        }
+
         // Line 클래스에 맞게 입력값 파싱
         for(int idx=0 ; idx<N ; idx++) {
             codes[idx] = toLine(idx);
         }
 
-        // 인덴트를 추가할지 감소할지 저장
-        boolean tabFlag = codes[0].tabFlag;
-        int start = 0;
-        int end = 0;
-        int max = 0;
-
-        while(start < N && end < N) {
-            // end 인덱스의 코드가 인덴트를 만족하지 않으면서, 기존의 코드와 tabFlag 가 동일할 때
-            if(!codes[end].satisfied && tabFlag == codes[end].tabFlag) {
-                max = Math.max(max, codes[end].move);
-                end++;
-            } else if (!codes[end].satisfied && tabFlag != codes[end].tabFlag) {
-                for(int idx=start ; idx<end; idx++) {
-                    // 모두 만족하게 바꾸고, max만큼 result를 증가
-                    codes[idx].satisfied = true;
-                }
-                result += max;
-                tabFlag = codes[end].tabFlag;
-                max = codes[end].move;
-
-                start = end;
-                end++;
-
-            } else {
-                if(start < end) {
-                    for(int idx=start ; idx<end; idx++) {
-                        // 모두 만족하게 바꾸고, max만큼 result를 증가
-                        codes[idx].satisfied = true;
-                    }
-                    result += max;
-                }
-                end++;
-                start = end;
-                max = 0;
-                tabFlag = codes[end].tabFlag;
+        for(int i=1 ; i<N ; i++) {
+            // 이전 코드와 현재 코드의 실행이 다른 경우
+            if(codes[i-1].tabFlag != codes[i].tabFlag) {
+                result += codes[i-1].move;
             }
-        } // while_start
-
-        if(max != 0) {
-            for(int idx=start ; idx<N; idx++) {
-                // 모두 만족하게 바꾸고, max만큼 result를 증가
-                codes[idx].satisfied = true;
+            // 실행은 같은데 이전 코드가 더 많이 또는 똑같이 행해야 하는경우
+            else if(codes[i-1].move >= codes[i].move) {
+                result += codes[i-1].move - codes[i].move;
             }
-            result += max;
         }
 
+        result += codes[N-1].move;
         System.out.println(result);
     }
 
