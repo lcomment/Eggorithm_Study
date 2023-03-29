765cdb2df6339be98f89ebcb2e8319eab18174ab
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-03-29 16:30:13 +0900

    보기 쉽게 수정

--- himJJong - Java/03.28/BOJ_7562.java
+++ himJJong - Java/03.28/BOJ_7562.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
 import java.util.*;
 
@@ -30,6 +28,7 @@ public class BOJ_7562 {
             answer = 0;
             length = Integer.parseInt(br.readLine());
             visited = new boolean[length][length];
+
             st = new StringTokenizer(br.readLine());
             int currentX = Integer.parseInt(st.nextToken());
             int currentY = Integer.parseInt(st.nextToken());
@@ -39,32 +38,36 @@ public class BOJ_7562 {
             int goalX = Integer.parseInt(st.nextToken());
             int goalY = Integer.parseInt(st.nextToken());
             Node goal = new Node(goalX,goalY,0);
+
             if(currentX == goalX && currentY == goalY) answer = 0;
             else bfs(current,goal,0);
+
             sb.append(answer).append("\n");
         }
         System.out.print(sb);
     }
 
     private static void bfs(Node current, Node goal, int time) {
-
         list.add(current);
+
         while(!list.isEmpty()){
             Node val = list.remove(0);
             visited[val.x][val.y] = true;
+
             for(int i=0; i<8;i++){
                 int moveX = val.x + dx[i];
                 int moveY = val.y + dy[i];
                 int time_val = val.time + 1;
+
                 if(goal.x == moveX && moveY == goal.y) {
                     list.clear();
                     answer = time_val;
                     break;
                 }
+
                 if(moveX>=0 && moveX<length && moveY>=0 && moveY<length && !visited[moveX][moveY]){
                     visited[moveX][moveY] = true;
                     list.add(new Node(moveX,moveY, time_val));
-
                 }
             }
         }
