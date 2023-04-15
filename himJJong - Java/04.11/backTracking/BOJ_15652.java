1e5886664592c15943c912f4755803bbb6000320
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-04-15 22:41:18 +0900

    N과M 1~4

---
+++ himJJong - Java/04.11/backTracking/BOJ_15652.java
@@ -0,0 +1,42 @@
+package backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_15652 {
+    static int[] NM;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        NM = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        answer = new int[NM[1]];
+
+        for(int i=1; i<=NM[0];i++){
+            answer[0] = i;
+            backTracking(i,0);
+        }
+        System.out.print(sb);
+    }
+
+    private static void backTracking(int at,int depth) {
+        if(depth+1 == NM[1]){
+            for(int i=0; i<answer.length; i++){
+                sb.append(answer[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=at; i<=NM[0] ; i++){
+            answer[depth+1] = i;
+            backTracking(i,depth+1);
+        }
+    }
+}
