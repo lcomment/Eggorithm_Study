8300410b70486c2483cb063fcc40b3832a552e7f
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-08-22 21:44:03 +0900

    BOJ_2564 경비원 이해

--- himJJong - Java/23.08.16/BOJ_2564.java
+++ himJJong - Java/23.08.16/BOJ_2564.java
@@ -1,83 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class BOJ_2564 {
-    static int[][] user = new int[1][2];
-    static int answer = 0;
-    static int N;
-    static int M;
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    static int X, Y, stores, a, b, loc, ans;
+    static int[] arr;
 
-        int count = Integer.parseInt(br.readLine());
-
-        int[][] data = new int[count][2];
-
-        for(int i=0; i<count; i++){
-            st = new StringTokenizer(br.readLine());
-            data[i][0] = Integer.parseInt(st.nextToken());
-            data[i][1] =Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt(), cnt = 0, police=0;
+        int[] map = new int [k];
+        for (int i = 0; i < k+1; i++) {
+            int dir = sc.nextInt(), loc = sc.nextInt(), tmp=0;
+            switch (dir) {
+                case 1:
+                    tmp = loc;
+                    break;
+                case 2:
+                    tmp = n + m + n - loc;
+                    break;
+                case 3:
+                    tmp = n + m + n + m - loc;
+                    break;
+                case 4:
+                    tmp = n + loc;
+                    break;
+            }
+            if(i<k)
+                map[i]=tmp;
+            else
+                police=tmp;
         }
-        st = new StringTokenizer(br.readLine());
-        user[0][0] = Integer.parseInt(st.nextToken());
-        user[0][1] = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<count; i++){
-            if(user[0][0] == data[i][0]){
-                answer += Math.abs(user[0][1] - data[i][1]);
-                continue;
-            }
-            if(user[0][0] == 1){
-                if(data[i][0] == 2){
-                    answer += Math.min(user[0][1] + data[i][1], (N-user[0][1]) + (N-data[i][1])) + M;
-                }
-                else if(data[i][0] == 3){
-                    answer += data[i][1] + user[0][1];
-                }
-                else if(data[i][0] == 4){
-                    answer += data[i][1] + (N-user[0][1]);
-                }
-            }
-            else if(user[0][0] == 2){
-                if(data[i][0] == 1){
-                    answer += Math.min(user[0][1] + data[i][1], (N-user[0][1]) + (N-data[i][1])) + M;
-                }
-                else if(data[i][0] == 3){
-                    answer += (M-data[i][1]) + user[0][1];
-                }
-                else if(data[i][0] == 4){
-                    answer += (M-data[i][1]) + (N-user[0][1]);
-                }
-            }
-            else if(user[0][0] == 3){
-                if(data[i][0] == 2){
-                    answer += data[i][1] + (M-user[0][1]);
-                }
-                else if(data[i][0] == 1){
-                    answer += (user[0][1]) + data[i][1];
-                }
-                else if(data[i][0] == 4){
-                    answer += Math.min(user[0][1] + data[i][1], (M-user[0][1]) + (M-data[i][1])) + N;
-                }
-            }
-            else if(user[0][0] == 4){
-                if(data[i][0] == 2){
-                    answer += (N - data[i][1]) + (M - user[0][1]);
-                }
-                else if(data[i][0] == 3){
-                    answer += Math.min(user[0][1] + data[i][1], (M-user[0][1]) + (M-data[i][1])) + N;
-                }
-                else if(data[i][0] == 1){
-                    answer += user[0][1] + (N-data[0][1]);
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            int path1=Math.abs(police-map[i]);
+            int path2 = 2*n+2*m-path1;
+            cnt += Math.min(path1, path2);
         }
-        System.out.println(answer);
+        System.out.println(cnt);
     }
 }
---
+++ himJJong - Java/23.08.16/BOJ_2668.java
@@ -0,0 +1,5 @@
+public class BOJ_2668 {
+    public static void main(String[] args) {
+
+    }
+}
