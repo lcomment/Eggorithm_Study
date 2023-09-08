614eb87b3bd171c23458111a912c20e46c961db4
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-09-08 14:00:50 +0900

    codeTree_Stack

--- himJJong - Java/23.09.05/codeTree_twopointer.java
+++ himJJong - Java/23.09.05/codeTree_twopointer.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class codeTree_twopointer {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
