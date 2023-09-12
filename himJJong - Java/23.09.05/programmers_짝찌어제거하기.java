32316bcce75c079bf356fdecef9afad33850cf1a
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-09-12 15:10:32 +0900

    programmers_짝지어제거하기

---
+++ himJJong - Java/23.09.05/programmers_짝찌어제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution{
+    public int solution(String s){
+        Stack<String> st = new Stack<>();
+        String[] tmp = s.split("");
+        int index = 0;
+        st.add(tmp[index]);      //만약 s가 1개라면 오류
+
+        if(s.length() == 1) return 0;
+        index++;
+        while(index != s.length()){
+            if(!st.isEmpty() && st.peek().equals(tmp[index])){
+                st.pop();
+            }
+            else{
+                st.add(tmp[index]);
+            }
+            index++;
+        }
+
+        if(st.isEmpty()) {
+            return 1;
+        }
+        return 0;
+    }
+}
