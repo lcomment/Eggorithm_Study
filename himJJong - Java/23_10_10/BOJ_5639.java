e0d7aa0cc890e73d3e5360d3ff68fa88e49b95a3
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-10-11 16:42:21 +0900

    BOJ_5639

---
+++ himJJong - Java/23_10_10/BOJ_5639.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_5639 {
+    static class Node{
+        int num;
+        Node left, right;
+
+        Node(int num) {
+            this.num = num;
+        }
+        void insert(int n){
+            if(n < this.num){
+                if(this.left == null)   this.left = new Node(n);
+                else this.left.insert(n);
+            }
+            else{
+                if(this.right == null)  this.right = new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        String input;
+        while(true){
+            input = br.readLine();
+            if(input == null || input.isEmpty())
+                break;
+            root.insert(Integer.parseInt(input));
+        }
+
+        postOrder(root);
+    }
+
+    private static void postOrder(Node node) {
+        if(node == null)    return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.num);
+
+    }
+}
