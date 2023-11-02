254fa29753ab02c90746f77e5276532f096f6850
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-11-02 14:46:49 +0900

    softeer_score_level3

---
+++ himJJong - Java/23_11_01/softeer_score_level3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class softeer_score_level3 {
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        double[] sum = new double[N];
+        st = new StringTokenizer(br.readLine());
+        sum[0] = Double.parseDouble(st.nextToken());
+        for(int i=1; i<N; i++){
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<K; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+
+            if(a == 0){
+                System.out.println(String.format("%.2f", sum[b]/(b-a+1)));
+            }
+            else{
+                System.out.println(String.format("%.2f", (sum[b] - sum[a-1])/(b-a+1)));
+            }
+        }
+    }
+}
