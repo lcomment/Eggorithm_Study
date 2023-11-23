6eef5357c75bf78348e613a4b5631415625611bd
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-11-23 19:13:26 +0900

    test

--- himJJong - Java/23_11_21/test2_dfs.java
+++ himJJong - Java/23_11_21/test2_dfs.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class test2_dfs {
     static class Node {
         int value;
@@ -9,27 +10,37 @@ public class test2_dfs {
             this.children = new ArrayList<>();
         }
     }
+
     public static void main(String[] args) {
-        int n = 6;
-        int[][] relationships = {{1, 2}, {1, 3}, {3, 6}, {3, 4}, {3, 5}};
-        setNodeValues(n, relationships);
-    }
-    public static void setNodeValues(int n, int[][] relationships) {
+        int n = 8;
+        int[][] relationships = {{2, 1}, {2, 3}, {3, 6}, {3, 4}, {3, 5}, {1, 7}, {1, 8}};
         Node[] nodes = new Node[n + 1];
 
         for (int i = 1; i <= n; i++) {
             nodes[i] = new Node(0);
         }
 
-        // 그래프 생성
+        boolean[] isChild = new boolean[n + 1];
+
+        // 그래프 생성 및 자식 여부 체크
         for (int[] relation : relationships) {
             int parentValue = relation[0];
             int childValue = relation[1];
             nodes[parentValue].children.add(nodes[childValue]);
+            isChild[childValue] = true;
+        }
+
+        // 최상위 노드(root) 찾기
+        Node root = null;
+        for (int i = 1; i <= n; i++) {
+            if (!isChild[i]) {
+                root = nodes[i];
+                break;
+            }
         }
 
         // DFS 호출
-        dfs(nodes[1]);
+        dfs(root);
 
         // 결과 출력
         for (int i = 1; i <= n; i++) {
@@ -37,16 +48,18 @@ public class test2_dfs {
         }
     }
 
-    public static void dfs(Node node) {
+    public static int dfs(Node node) {
         if (node.children.isEmpty()) {
             node.value = 1;
-            return;
+            return 1;
         }
 
+        int childSum = 0;
         for (Node child : node.children) {
-            dfs(child);
-            node.value += child.value;
+            childSum += dfs(child);
         }
-        node.value += 1; // 자식 노드의 값의 합에 현재 노드 값(1)을 더함
+
+        node.value = childSum;
+        return childSum;
     }
 }
