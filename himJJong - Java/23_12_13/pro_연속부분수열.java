b6e1a30c1cf25142d9edaa1c845f5b9a26348dbd
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-12-15 17:22:57 +0900

    pro_연속부분수열

--- himJJong - Java/23_12_13/pro_연속부분수열.java
+++ himJJong - Java/23_12_13/pro_연속부분수열.java
@@ -1,7 +1,7 @@
 public class pro_연속부분수열 {
     public static void main(String[] args) {
-        int[] sequence = {1,1,1,2,3,4,5};
-        int k = 5;
+        int[] sequence = {1,1,1,1,1,1,3};
+        int k = 3;
 
         int[] answer = new int[2];
         answer[1] = sequence.length-1;
@@ -13,10 +13,14 @@ public class pro_연속부분수열 {
 
         while(left < right){
             if(sum == k && (answer[1] - answer[0] > right - left-1)){
-                answer[1] = right;
+                answer[1] = right-1;
                 answer[0] = left;
 
                 sum -= sequence[left++];
+                if(right == sequence.length){
+                    right--;
+                }
+                sum += sequence[right++];
             }
             else if(sum > k){
                 sum -= sequence[left++];
@@ -26,5 +30,7 @@ public class pro_연속부분수열 {
             }
             else break;
         }
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
     }
 }
