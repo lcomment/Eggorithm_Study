d6a1005c2fc703999b95aea756ec955865d08a2f
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2024-01-02 17:14:37 +0900

    BOJ_17070

--- himJJong - Java/23_12_27/BOJ_1655.java
+++ himJJong - Java/23_12_27/BOJ_1655.java
@@ -1,24 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class BOJ_1655 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Queue<Integer> q = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int count = 9;
-        while(count-- > 0){
-            int tmp = Integer.parseInt(br.readLine());
-            pq.add(tmp);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for(int i = 0 ; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(minHeap.size() == maxHeap.size()) {
+                maxHeap.offer(num);
+            }
+            else {
+                minHeap.offer(num);
+            }
+
+            if(!minHeap.isEmpty() && !maxHeap.isEmpty())
+                if(minHeap.peek() < maxHeap.peek()){
+                    int tmp = minHeap.poll();
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(tmp);
+                }
+
+            sb.append(maxHeap.peek() + "\n");
         }
-        System.out.println(0);
-
+        System.out.println(sb);
     }
 }
